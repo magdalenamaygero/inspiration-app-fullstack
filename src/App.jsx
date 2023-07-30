d69cfeee263222bae16ed834e7c797bfa519357c
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-07-29 23:02:30 -0400

    new color theme, i node and express

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,26 @@ function App() {
   return (
     <>
       <nav>
+        <div class="level-right">
+         {/* <p class="level-item"><strong>All</strong></p> */}
+        {/* <p class="level-item"><a>Favorite</a></p> */}
+        <button class="button is-link is-light">Favorites</button>
+        </div>
         <h1 id="title">MIND @ EASE</h1>
         <h2 class="subtitle is-2" >Welcome To Your World Of Creativity</h2>
-        {/* <button class="button is-success">Home</button> */}
-        <button class="button is-success">Saved</button>
+        {/* <button class="button is-success">Saved</button> */}
       </nav>
       <main>
         <Quotes />
       </main>
-      {/* <div> */}
-        {/* <button onClick={}>Refresh</button> */}
-      {/* </div> */}
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Mind@Ease</strong> by <a href="https://jgthms.com">Magdalena May Gero </a> from
+            <a href="http://opensource.org/licenses/mit-license.php"> The Marcy Lab School</a>
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
